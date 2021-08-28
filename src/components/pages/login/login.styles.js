b8d92acc76efdcbbe51797fa20f33e399f403321
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2021-08-28 16:40:08 +0430

    change direction to rtl

--- src/components/pages/login/login.styles.js
+++ src/components/pages/login/login.styles.js
@@ -1,5 +1,10 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles((theme) => ({
-  root: { maxWidth: theme.spacing(55), margin: "auto", textAlign: "center" },
+  root: {
+    direction: "rtl",
+    maxWidth: theme.spacing(55),
+    margin: "auto",
+    textAlign: "center",
+  },
 }));
