33d2d344baf2abb284299886e8208c4fd067ddeb
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2021-08-28 16:24:06 +0430

    create Container component

--- src/components/pages/login/login.jsx
+++ src/components/pages/login/login.jsx
@@ -1,5 +1,7 @@
 import Localization from "../../../localization";
+// Components
+import ContainerBase from "../../items/container-base/container-base";
 
 export default function Login() {
-  return <div>{Localization.login_page_title_1}</div>;
+  return <ContainerBase>{Localization.login_page_title_1}</ContainerBase>;
 }
