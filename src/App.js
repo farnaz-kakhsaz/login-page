8242f2372aaa0fe30fdc07d0a55ec171f0baa1cf
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2021-08-29 01:25:10 +0430

    add responsive font size

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import Localization from "./localization";
-import { ThemeProvider } from "@material-ui/core/styles";
+import { ThemeProvider, responsiveFontSizes } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 // Component
 import Login from "./components/pages/login/login.component";
@@ -8,10 +8,12 @@ import Login from "./components/pages/login/login.component";
 import { theme } from "./theme/theme";
 import RTL from "./theme/right-to-left";
 
+let responsiveTheme = responsiveFontSizes(theme);
+
 function App() {
   useEffect(() => Localization.setLanguage("fa"), []);
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={responsiveTheme}>
       <RTL>
         <CssBaseline />
         <Login />
