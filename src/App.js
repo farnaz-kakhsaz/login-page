ed76b9a0f3a05aabe232ed63de230e2cededdff2
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2021-08-28 16:08:43 +0430

    add login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import Localization from "./localization";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
+// Component
+import Login from "./components/pages/login/login";
 // Theme
 import { theme } from "./theme/theme";
 
@@ -10,7 +12,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div>{Localization.login_page_title_1}</div>;
+      <Login />
     </ThemeProvider>
   );
 }
---
+++ src/components/pages/login/login.jsx
@@ -0,0 +1,5 @@
+import Localization from "../../../localization";
+
+export default function Login() {
+  return <div>{Localization.login_page_title_1}</div>;
+}
